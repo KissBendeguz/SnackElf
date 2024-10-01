e17e1208363beb79e8a5fc18e48d17eb81f90dc0
Kiss Bendegúz <kissbendeguz12@gmail.com>
2024-10-01 09:11:17 +0200

    Initial commit

---
+++ src/main/java/hu/elte/snackelf/SnackelfApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.snackelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/elte/snackelf/SnackelfApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.elte.snackelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
